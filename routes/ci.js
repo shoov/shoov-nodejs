c26a92176dea87772fa2ecc9621281d858693a2c
Amitai Burstein <amitai@gizra.com>
2015-04-11 21:22:54 +0300

    Add ci route

--- routes/ci.js
+++ routes/ci.js
@@ -1,41 +1,42 @@
 var express = require('express');
 var router = express.Router();
 var Promise = require('bluebird');
-var request = Promise.promisify(require('request'));
+var request = require('request-promise');
 var Docker = require('dockerode');
 
 // Invoke a PR.
-router.get('/:buildId/:accessToken', function(req, res, next) {
+router.get('/:buildItemId/:accessToken', function(req, res, next) {
 
-  var buildId = req.params.buildId;
+  var buildItemId = req.params.buildItemId;
   var accessToken = req.params.accessToken;
   var options = {
-    url: process.env.BACKEND_URL + '/api/ci-builds/' + buildId,
+    url: process.env.BACKEND_URL + '/api/ci-build-items/' + buildItemId,
     method: 'PATCH',
     qs: {
       access_token: accessToken
     },
     form: {
-      pull_request_status: 'in_progress'
+      status: 'in_progress'
     }
   };
 
   // Set the build status to "in progress".
   request(options)
     .then(function(response) {
-      return execDocker(buildId, accessToken);
+      var data = JSON.parse(response).data[0];
+      return execDocker(data.build, accessToken);
     })
     .then(function(data) {
       // @todo: Add validation (i.e. exit code) to the docker bash.
       console.log('Docker done');
       // Set the build status to "done".
-      options.form.pull_request_status = 'done';
+      options.form.status = 'done';
       return request(options);
     })
     .catch(function(err) {
       console.log(err);
       // Set the build status to "error".
-      options.form.pull_request_status = 'error';
+      options.form.status = 'error';
       return request(options);
     });
 
