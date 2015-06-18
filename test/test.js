cd795efc37485b49ba2c18ad1e63a920e4138b82
Ilya Manyahin <ilya@gizra.com>
2015-06-18 11:56:36 +0300

    remove node v0.9 from travis

--- test/test.js
+++ test/test.js
@@ -25,12 +25,12 @@ describe('Shoov NodeJS Server', function() {
   it('should return an encrypted value', function(done) {
     var options = {
       privateKey: 'Y2DCJ6zGnVvZamcusAuNs1HfC-4AqDU46rvMgRAclSM',
-      keyToConvert: 'GITHUB_DUMMY_USERNAME',
-      valueToConvert: 'shoov-tester'
+      keyToConvert: 'foo',
+      valueToConvert: 'bar'
     };
     request.post({url: url + '/encrypt', form: options}, function(err, httpResponse, body) {
       if (err) throw err;
-      assert.equal("f14b9cdbd14039ea3b230becca0c15d7ac47cc6aee31e356a7a71a0f7b2376b1e304", JSON.parse(body).encrypt);
+      assert.equal("d06da7a9e66314", JSON.parse(body).encrypt);
       done();
     });
   });
